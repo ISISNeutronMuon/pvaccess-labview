d3f6eb17bfe1e05badd3cdadce83ce11a1a6e6bc
Ross Titmarsh <23349806+rosstitmarsh@users.noreply.github.com>
2026-03-11 18:00:03 +0000

    Changed windows build to static and fixed linux build

--- cmake/epics-base.cmake
+++ cmake/epics-base.cmake
@@ -1,14 +1,25 @@
 set(EPICS_BASE_ROOT "${CMAKE_SOURCE_DIR}/epics-base")
 set(EPICS_BASE_INCLUDE_DIRS "${EPICS_BASE_ROOT}/include")
 
-add_library(Com STATIC IMPORTED)
-target_include_directories(Com INTERFACE "${EPICS_BASE_INCLUDE_DIRS}")
-set_target_properties(Com PROPERTIES IMPORTED_LOCATION "${EPICS_BASE_ROOT}/lib/$ENV{EPICS_HOST_ARCH}/${CMAKE_STATIC_LIBRARY_PREFIX}Com${CMAKE_STATIC_LIBRARY_SUFFIX}")
 if(WIN32)
+  add_library(Com STATIC IMPORTED)
+  set_target_properties(Com PROPERTIES
+    IMPORTED_LOCATION
+      "${EPICS_BASE_ROOT}/lib/$ENV{EPICS_HOST_ARCH}/${CMAKE_STATIC_LIBRARY_PREFIX}Com${CMAKE_STATIC_LIBRARY_SUFFIX}"
+  )
   target_include_directories(Com INTERFACE "${EPICS_BASE_INCLUDE_DIRS}/os/win32")
 else()
+  add_library(Com SHARED IMPORTED)
+  set(EPICS_BASE_LIB_NAME "${CMAKE_SHARED_LIBRARY_PREFIX}Com${CMAKE_SHARED_LIBRARY_SUFFIX}")
+  set_target_properties(Com PROPERTIES
+    IMPORTED_SONAME
+      "${EPICS_BASE_LIB_NAME}"
+    IMPORTED_LOCATION
+      "${EPICS_BASE_ROOT}/lib/$ENV{EPICS_HOST_ARCH}/${EPICS_BASE_LIB_NAME}.3.25.0"
+  )
   target_include_directories(Com INTERFACE "${EPICS_BASE_INCLUDE_DIRS}/os/Linux")
 endif()
+target_include_directories(Com INTERFACE "${EPICS_BASE_INCLUDE_DIRS}")
 if(MSVC)
   target_include_directories(Com INTERFACE "${EPICS_BASE_INCLUDE_DIRS}/compiler/msvc")
 else()
--- cmake/pvxs.cmake
+++ cmake/pvxs.cmake
@@ -1,12 +1,25 @@
 set(PVXS_ROOT "${CMAKE_SOURCE_DIR}/pvxs")
 
-add_library(pvxs STATIC IMPORTED)
-target_include_directories(pvxs INTERFACE "${PVXS_ROOT}/include")
-set_target_properties(pvxs PROPERTIES
-  IMPORTED_LOCATION "${PVXS_ROOT}/lib/$ENV{EPICS_HOST_ARCH}/${CMAKE_STATIC_LIBRARY_PREFIX}pvxs${CMAKE_STATIC_LIBRARY_SUFFIX}"
-)
+if(WIN32)
+  add_library(pvxs STATIC IMPORTED)
+  set_target_properties(pvxs PROPERTIES
+    IMPORTED_LOCATION
+      "${PVXS_ROOT}/lib/$ENV{EPICS_HOST_ARCH}/${CMAKE_STATIC_LIBRARY_PREFIX}pvxs${CMAKE_STATIC_LIBRARY_SUFFIX}"
+  )
 
-add_library(event_core STATIC IMPORTED)
-set_target_properties(event_core PROPERTIES
-  IMPORTED_LOCATION "${PVXS_ROOT}/bundle/usr/$ENV{EPICS_HOST_ARCH}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}event_core${CMAKE_STATIC_LIBRARY_SUFFIX}"
-)
+  add_library(event_core STATIC IMPORTED)
+  set_target_properties(event_core PROPERTIES
+    IMPORTED_LOCATION
+      "${PVXS_ROOT}/bundle/usr/$ENV{EPICS_HOST_ARCH}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}event_core${CMAKE_STATIC_LIBRARY_SUFFIX}"
+  )
+else()
+  add_library(pvxs SHARED IMPORTED)
+  set(PVXS_LIB_NAME "${CMAKE_SHARED_LIBRARY_PREFIX}pvxs${CMAKE_SHARED_LIBRARY_SUFFIX}")
+  set_target_properties(pvxs PROPERTIES
+    IMPORTED_SONAME
+      "${PVXS_LIB_NAME}"
+    IMPORTED_LOCATION
+      "${PVXS_ROOT}/lib/$ENV{EPICS_HOST_ARCH}/${PVXS_LIB_NAME}.1.5"
+  )
+endif()
+target_include_directories(pvxs INTERFACE "${PVXS_ROOT}/include")
